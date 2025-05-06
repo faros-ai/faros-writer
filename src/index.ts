03fc4eeff902e591373d230a906a77e3d8217c16
Ron Meldiner <ron@faros.ai>
2025-05-05 17:28:11 -0700

    ingest ai token use example

--- src/index.ts
+++ src/index.ts
@@ -9,84 +9,40 @@ import {csvReadRows, farosReadNodes} from './utils';
 
 const faros_api_url = process.env.FAROS_API_URL || 'https://prod.api.faros.ai';
 const faros_api_key = process.env.FAROS_API_KEY || '<key>';
-const graph = process.env.FAROS_GRAPH || 'default';
+const graph = process.env.FAROS_GRAPH || 'test';
 const origin = process.env.FAROS_ORIGIN || 'faros-writer';
 const maxBatchSize = Number(process.env.FAROS_BATCH_SIZE) || 500;
 const debug = (process.env.FAROS_DEBUG || 'true') === 'true';
+
+const projectToTeam = {
+  'AI POD': 'team-uid',
+}
 
 async function* mutations(faros: FarosClient): AsyncGenerator<Mutation> {
   // The QueryBuilder manages the origin for you
   const qb = new QueryBuilder(origin);
 
-  // EXAMPLE 1: Construct your models manually, yielding mutations...
-  const compute_Application = {
-    name: '<application_name>',
-    platform: '<application_platform>',
-  };
-  const cicd_Deployment = {
-    uid: '<deployment_uid',
-    source: '<deployment_source>',
-    // Fields that reference another model need to be refs
-    application: qb.ref({compute_Application}),
-    status: {
-      category: 'Success',
-      detail: '<status_detail>',
-    },
-  };
-  // Yield mutations, Batching will be handled for you
-  yield qb.upsert({compute_Application});
-  yield qb.upsert({cicd_Deployment});
-
-  // EXAMPLE 2: Iterate across all rows in a CSV file, yielding mutations...
-  for await (const row of csvReadRows('../resources/example.csv')) {
-    const compute_Application = {
-      name: row.application,
-      platform: '',
-    };
-    const cicd_Deployment = {
-      uid: row.deployment_id,
-      source: row.deployment_source,
-      application: qb.ref({compute_Application}),
-      status: {
-        category: row.status,
-        detail: '',
-      },
-    };
-    yield qb.upsert({compute_Application});
-    yield qb.upsert({cicd_Deployment});
-  }
-
-  // EXAMPLE 3: Iterate across a Faros graph query, yielding mutations...
-  const query = `
-  {
-    cicd_Deployment {
-      uid
-      source
-      application {
-        name
-        platform
-      }
-      status
+  for await (const row of csvReadRows('../resources/ai-tokens.csv')) {
+    const vcs_User = {
+      uid: row.Username
     }
-  }
-  `;
-  const farosNodes = farosReadNodes(faros, graph, query);
-  for await (const node of farosNodes) {
-    const compute_Application = {
-      name: node.application.name,
-      platform: node.application.platform,
+    yield qb.upsert({vcs_User});
+
+
+    const vcs_Team = {
+      uid: projectToTeam[row['Project Name']] || row['Project Name'],
+    }
+    yield qb.upsert({vcs_Team});
+
+    const vcs_AssistantMetric = {
+      uid: `${row.Date}-${row.Username}-RequestDWPromptTokens`,
+      value: row.RequestDWPromptTokens,
+      startedAt: row.Date,
+      endedAt: row.Date,
+      user: qb.ref({vcs_User}),
+      team: qb.ref({vcs_Team}),
     };
-    const cicd_Deployment = {
-      uid: node.uid,
-      source: node.source,
-      application: qb.ref({compute_Application}),
-      status: {
-        category: node.status.category,
-        detail: node.status.detail,
-      },
-    };
-    yield qb.upsert({compute_Application});
-    yield qb.upsert({cicd_Deployment});
+    yield qb.upsert({vcs_AssistantMetric});
   }
 }
 
